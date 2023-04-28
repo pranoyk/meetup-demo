c1ff7dea0f134e5886c74e4cf3b797d18411d512
avinilcodes <avinilbedarkar@gmail.com>
2023-04-28 12:48:57 +0530

    initial commit

---
+++ service-provider/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"storj.io/common/uuid"
+)
+
+type ServiceProvider struct {
+	ID       string `json:"id" bson:"_id"`
+	Name     string `json:"name" bson:"name"`
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+}
+
+type Menu struct {
+	ID    string `json:"id" bson:"_id"`
+	Gravy string `json:"gravy" bson:"gravy"`
+}
+
+func main() {
+	// Set up MongoDB connection
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	// Set up Gin router
+	router := gin.Default()
+
+	// ServiceProvider registration endpoint
+	router.POST("/serviceProviders", func(c *gin.Context) {
+		// Parse request body
+		var serviceProvider ServiceProvider
+		if err := c.ShouldBindJSON(&serviceProvider); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Generate new serviceProvider ID
+		ID, err := uuid.New()
+		if err != nil {
+			return
+		}
+
+		serviceProvider.ID = ID.String()
+
+		// Insert serviceProvider into MongoDB
+		serviceProvidersCollection := client.Database("serviceProvider-management").Collection("serviceProviders")
+		_, err = serviceProvidersCollection.InsertOne(context.Background(), serviceProvider)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, serviceProvider)
+	})
+
+	// ServiceProvider authentication endpoint
+	router.POST("/serviceProviders/login", func(c *gin.Context) {
+		// Parse request body
+		var serviceProvider ServiceProvider
+		if err := c.ShouldBindJSON(&serviceProvider); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Check if serviceProvider exists in MongoDB
+		serviceProvidersCollection := client.Database("serviceProvider-management").Collection("serviceProviders")
+		filter := bson.M{"email": serviceProvider.Email, "password": serviceProvider.Password}
+		err := serviceProvidersCollection.FindOne(context.Background(), filter).Decode(&serviceProvider)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+			return
+		}
+
+		c.JSON(http.StatusOK, serviceProvider)
+	})
+
+	router.POST("/serviceProviders/menu", func(c *gin.Context) {
+		// Parse request body
+		var menu Menu
+		if err := c.ShouldBindJSON(&menu); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Check if serviceProvider exists in MongoDB
+		serviceProvidersCollection := client.Database("serviceProvider-management").Collection("serviceProviders")
+		filter := bson.M{"gravy": menu.Gravy}
+		err := serviceProvidersCollection.FindOne(context.Background(), filter).Decode(&menu)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+			return
+		}
+
+		c.JSON(http.StatusOK, menu)
+	})
+
+	// Run Gin server
+	if err := router.Run(":8088"); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ user/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"storj.io/common/uuid"
+)
+
+type User struct {
+	ID       string `json:"id" bson:"_id"`
+	Name     string `json:"name" bson:"name"`
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+}
+
+func main() {
+	// Set up MongoDB connection
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	// Set up Gin router
+	router := gin.Default()
+
+	// User registration endpoint
+	router.POST("/users", func(c *gin.Context) {
+		// Parse request body
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Generate new user ID
+		ID, err := uuid.New()
+		if err != nil {
+			return
+		}
+
+		user.ID = ID.String()
+
+		// Insert user into MongoDB
+		usersCollection := client.Database("user-management").Collection("users")
+		_, err = usersCollection.InsertOne(context.Background(), user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, user)
+	})
+
+	// User authentication endpoint
+	router.POST("/users/login", func(c *gin.Context) {
+		// Parse request body
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Check if user exists in MongoDB
+		usersCollection := client.Database("user-management").Collection("users")
+		filter := bson.M{"email": user.Email, "password": user.Password}
+		err := usersCollection.FindOne(context.Background(), filter).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+	})
+
+	// Run Gin server
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
